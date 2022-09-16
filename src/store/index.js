1b153c47d13f99f21eeb25c2e43b921d847c2f1f
wes337 <wesmoses@gmail.com>
2022-09-16 09:02:30 +0300

    try conditional chaining in process

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore } from "solid-js/store";
 import initialState from "./initialState";
 
-//const ws = new WebSocket("wss://mondo-megabits.herokuapp.com");
-const ws = new WebSocket(process.env.WS_SERVER);
-
-console.log(process.env);
+//const ws = new WebSocket("");
+const ws = new WebSocket(
+  process?.env?.WS_SERVER || "wss://mondo-megabits.herokuapp.com"
+);
 
 function useStore() {
   const [state, setState] = createStore(initialState);
